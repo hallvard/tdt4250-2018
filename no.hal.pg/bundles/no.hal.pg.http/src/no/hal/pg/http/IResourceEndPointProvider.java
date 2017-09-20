ff4b4c4473a798b70b4f379b60a33781ed9ee6a6
Hallvard Traetteberg <hal@ntnu.no>
2017-09-20 17:58:42 +0200

    Removals.

--- no.hal.pg/bundles/no.hal.pg.http/src/no/hal/pg/http/IResourceEndPointProvider.java
+++
@@ -1,6 +0,0 @@
-package no.hal.pg.http;
-
-public interface IResourceEndPointProvider {
-	public void addResourceProvider(IResourceProvider resourceProvider);
-	public void removeResourceProvider(IResourceProvider resourceProvider);
-}
--- no.hal.pg/bundles/no.hal.pg.http/src/no/hal/pg/http/auth/AuthenticationHandlerProvider.java
+++
@@ -1,7 +0,0 @@
-package no.hal.pg.http.auth;
-
-import org.eclipse.emf.ecore.EObject;
-
-public interface AuthenticationHandlerProvider {
-	public AuthenticationHandler<?> getAuthenticationHandler(EObject context);
-}
